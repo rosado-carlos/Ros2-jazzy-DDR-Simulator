2fdfb0b4bacc439d8f8d520b6655d5ffeef31552
Carlos Rosado <carlosalbertorosado1412@gmail.com>
2026-04-08 17:31:58 -0500

    Better result with 90 grades walls

--- src/echo_wall_following/echo_wall_following/control.py
+++ src/echo_wall_following/echo_wall_following/control.py
@@ -17,7 +17,7 @@ class WallFollowerControl(Node):
         # -------- Parameters --------
         self.declare_parameter('kp', 1.1)
         self.declare_parameter('kd', 1.5)
-        self.declare_parameter('max_steering', 3.0)  # Límite de dirección
+        self.declare_parameter('max_steering', 2.2)  # Límite de dirección
 
         self.declare_parameter('max_velocity', 1.7)
         self.declare_parameter('min_velocity', 1.33)
@@ -35,7 +35,7 @@ class WallFollowerControl(Node):
         self.prev_error = 0.0
         self.prev_time = time.time()
         self.front_yaw = 0.0  # Distancia frontal para detección de obstáculos
-        self.diagiz_state = False  # Estado del sensor diagonal para detección de esquinas
+        self.diagiz = 0.0
         self.dist_min = float('inf')  # Distancia mínima frontal
         self.rb_pressed = False  # Estado del botón RB del joystick
 
@@ -124,13 +124,15 @@ class WallFollowerControl(Node):
 
     def front_callback(self, msg):
         self.dist_min = msg.linear.x
-        if msg.linear.x < 2.5 and msg.linear.y > 1.0 and self.diagiz_state:
-            self.front_yaw = 0.6 * msg.linear.y + 0.5/(msg.linear.x*msg.linear.x)
+        if msg.linear.x < 2.5 and msg.linear.y > 1.0 and self.diagiz > self.dist_min:
+            self.front_yaw = 0.6 * msg.linear.y + 0.65/(msg.linear.x*msg.linear.x)
+            if self.diagiz - self.dist_min < 0.4:
+                self.front_yaw += 1.5 * (self.diagiz - self.dist_min)
         else:
             self.front_yaw = 0.0
     
     def dist_diagiz_callback(self, msg):
-        self.diagiz_state = msg.data > self.dist_min
+        self.diagiz = msg.data
 
     def joy_callback(self, msg):
         # Handle joystick messages if needed
